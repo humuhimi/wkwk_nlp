2b499a4a2c8ee8496e65004c53943b25a31f8fea
wmichi <waku.michi1227@gmail.com>
2019-07-03 11:52:11 +0900

    出力先とwithの使い方を変更

--- Chapter2/excecise2_3.py
+++ Chapter2/excecise2_3.py
@@ -6,22 +6,17 @@
 #  一列目をcol1.txtに，2列目をcol2.txtに変換する
 
 path_r = "hightemp.txt"
-path_w1 = "col1.txt"
-path_w2 = "col2.txt"
+path_w1 = "result/col1.txt"
+path_w2 = "result/col2.txt"
 
 
-lines = ''
-# with open(path_r,'r') as f:
-    # with open(path_w1,'w') as w1:
-    #     with open(path_w2,'w') as w2:
-with open(path_r,'r') as f,\
-     open(path_w1,mode='w') as w1,\
-     open(path_w2,mode='w') as w2:
-            for line in f:
-                # 高知県  江川崎  41      2013-08-12
-                line = line.split('\t')
-                w1.write(str(line[0]+'\n'))
-                w2.write(str(line[1]+'\n'))
+lines = ""
 
-          
-        
+with open(path_r, "r") as f, open(path_w1, mode="w") as w1, open(
+    path_w2, mode="w"
+) as w2:
+    for line in f:
+        # 高知県  江川崎  41      2013-08-12
+        line = line.split("\t")
+        w1.write(str(line[0] + "\n"))
+        w2.write(str(line[1] + "\n"))
