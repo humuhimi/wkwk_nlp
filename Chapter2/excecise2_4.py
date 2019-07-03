204ce321e21c6d43f074dffe9067a271d6c39c41
wmichi <waku.michi1227@gmail.com>
2019-07-03 11:53:15 +0900

    for文を二回回したくない

--- Chapter2/excecise2_4.py
+++ Chapter2/excecise2_4.py
@@ -2,32 +2,20 @@
 13. col1.txtとcol2.txtをマージ
 12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ
 """
-path_w1 = "col1.txt"
-path_w2 = "col2.txt"
-path_f1_2 = "col1_2ans.txt"
-w1_line = []
-w2_line = []
-
 import re
 
-with open(path_w1,'r') as w1:
-    with open(path_w2,'r') as w2:
-        for _w1 in w1:
-            w1_line.append(_w1)
-        for _w2 in w2:
-            w2_line.append(_w2)
+path_w1 = "result/col1.txt"
+path_w2 = "result/col2.txt"
+path_f1_2 = "result/col1_2ans.txt"
+combined_line = []
+
+with open(path_w1, "r") as w1, open(path_w2, "r") as w2:
+    for _w1, _w2 in zip(w1, w2):
+        combined_line.append(_w1.rstrip("\r\n") + "\t" + _w2)
+
+print("".join(combined_line))
 
 
-# print(w1_line[:]+w2_line[:])
-
-for i in range(len(w1_line)):
-    w1_line[i] += w2_line[i]
-    w1_line[i] = re.sub(r'\n','\t',w1_line[i],1)
-
-print(w1_line)
-
-
-with open(path_f1_2,'w') as res:
-    for w in w1_line:
+with open(path_f1_2, "w") as res:
+    for w in combined_line:
         res.write(w)
-        
